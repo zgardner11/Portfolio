5659245da94d4341a6532b32de50191949ef80f0
zgardner11 <zgardner.cinema@gmail.com>
2024-08-15 12:04:35 -0700

    Mobile updates, font color change

--- Scripts/controls.js
+++
@@ -1,9 +0,0 @@
-// Target element in elements
-
-// Play element on click
-
-// Pause element on click
-
-document.getElementById('video').onclick = function () {
-    document.getElementById('video').play();
-};
